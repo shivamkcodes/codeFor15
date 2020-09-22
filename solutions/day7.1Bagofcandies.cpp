d5472fce9f2c4899ab9ce6d2170c187f158ae91c
shivam <shivamguglani306@gmail.com>
2020-09-23 00:33:39 +0530

    day8

---
+++ solutions/day7.1Bagofcandies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v(4);
+    for (int &it : v)
+        cin >> it;
+    sort(v.begin(), v.end());
+    if (((v.at(0) + v.at(3)) == (v.at(1) + v.at(2))) or ((v.at(0) + v.at(1) + v.at(2)) == v.at(3)))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
