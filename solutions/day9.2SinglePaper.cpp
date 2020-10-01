0b7b4c5c72e9ca8f2c1961d97e8af6a49830f81d
shivam <shivamguglani306@gmail.com>
2020-10-01 23:23:17 +0530

    day10

---
+++ solutions/day9.2SinglePaper.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    int finalVal = 0;
+    cin >> n;
+    vector<int> v(n);
+    for (int &it : v)
+        cin >> it;
+    for (int &it : v)
+        finalVal ^= it;
+    cout << finalVal << endl;
+    return 0;
+}
